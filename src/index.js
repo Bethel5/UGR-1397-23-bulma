c5be6d7994ae9ed93710c0132ec0889e09f76a85
Bethel5 <betheldereje5@gmail.com>
2023-12-18 22:06:27 +0300

    add bulma css framework

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//hamburger for phone
+const burgerIcon = document.querySelector('#burger');
+const navbarmenu = document.querySelector('#nav-links');
+
+
+burgerIcon.addEventListener('click', () => {
+    navbarmenu.classList.toggle('is-active');
+});
+
